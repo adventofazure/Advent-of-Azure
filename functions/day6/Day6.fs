2fcbe7c94de6019a3428b2305c76827d6b38f925
adventofazure <adventofazure@gmail.com>
2020-12-07 22:22:41 +0200

    Added the solution for day 6 as Azure function in F#.

--- functions/day6/Day6.fs
+++ functions/day6/Day6.fs
@@ -10,6 +10,50 @@ open Newtonsoft.Json
 open Microsoft.Extensions.Logging
 
 module Day6 =
+    let getCountPart1 (log: ILogger) (groupAnswers: string) =
+        let answers =
+            groupAnswers.Split(",")
+            |> List.ofArray
+            |> List.fold (+) ""
+            |> Seq.toList
+            |> List.fold (fun (acc: Set<char>) (a: char) -> if acc.Contains a then acc else acc.Add a) Set.empty
+        answers.Count
+
+    // Solve Advent of Code 2020, day 6, part 1
+    let solvePart1 (log: ILogger) (input: string) =
+        log.LogInformation("Solving part 1")
+        input.Split(",,")
+        |> List.ofArray
+        |> List.sumBy (fun a -> getCountPart1 log a )
+        |> string
+
+    let getCountPart2 (log: ILogger) (groupAnswers: string) =
+        let answers =
+            groupAnswers.Split(",")
+            |> List.ofArray
+        let personCount = answers.Length
+
+        let handleAnswer (acc: Map<char, int>) (x: char) =
+            match acc.TryGetValue x with
+              | true, value -> acc.Add(x, value + 1)
+              | _ -> acc.Add(x, 1)
+
+        let answerMap =
+            answers
+            |> List.fold (+) ""
+            |> Seq.toList
+            |> List.fold handleAnswer Map.empty
+        let filteredMap = answerMap |> Map.filter (fun a b -> b = personCount)
+        filteredMap.Count
+
+    // Solve Advent of Code 2020, day 6, part 2
+    let solvePart2 (log: ILogger) (input: string) =
+        log.LogInformation("Solving part 2")
+        input.Split(",,")
+        |> List.ofArray
+        |> List.sumBy (fun a -> getCountPart2 log a )
+        |> string
+
     // Define a nullable container to deserialize into.
     [<AllowNullLiteral>]
     type InputContainer() =
@@ -47,7 +91,7 @@ module Day6 =
                 if (String.IsNullOrWhiteSpace(input)) then
                     "Day 6. No input given."
                 else
-                    "Day 6 is not implemented yet. Your input is " + input
+                    "Part 1: " + (solvePart1 log input) + "\n Part 2: " + (solvePart2 log input)
 
             return OkObjectResult(responseMessage) :> IActionResult
         } |> Async.StartAsTask
